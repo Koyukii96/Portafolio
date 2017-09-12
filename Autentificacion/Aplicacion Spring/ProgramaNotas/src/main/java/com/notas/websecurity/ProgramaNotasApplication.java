71a99b2f768704efec44d53b76772b4a04fad93a
Koyukii96 <Koyukii96@users.noreply.github.com>
2017-09-12 16:58:36 -0300

    Login

---
+++ Autentificacion/Aplicacion Spring/ProgramaNotas/src/main/java/com/notas/websecurity/ProgramaNotasApplication.java
@@ -0,0 +1,12 @@
+package com.notas.websecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaNotasApplication.class, args);
+	}
+}
